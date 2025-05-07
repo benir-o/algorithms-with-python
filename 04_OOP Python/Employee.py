c76716cc9fa05acf3198ec6670f988e5c98f77a3
benir-o <beniromenda@gmail.com>
2025-05-07 22:11:43 +0300

    Addition of class methods

--- 04_OOP Python/Employee.py
+++ 04_OOP Python/Employee.py
@@ -1,1 +1,27 @@
 # Employee class
+class Employee:
+    num_of_emps=0
+    raise_amount=1.04
+    def __init__(self,first,last,pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+        self.email=first+'.'+last+'@company.com'
+        Employee.num_of_emps+=1
+
+    def fullname(self):
+        return "{} {}".format(self.first,self.last)
+
+    def apply_raise(self):
+        self.pay=int(self.pay * self.raise_amount)
+
+emp_1=Employee('Benir','Odeny',60000)
+emp_2=Employee('Test','User',50000)
+print(emp_1.fullname())
+print(emp_2.fullname())
+# class variables - shared among all instances of a class
+
+print(Employee.num_of_emps)
+
+
+
